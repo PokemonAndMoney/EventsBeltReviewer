44f022142a33bacf46ada6e3dcd65149d3f4f1d3
Jason Cothran <healingfox115@gmail.com>
2021-09-15 10:51:13 -0500

    Initial commit

---
+++ src/test/java/com/jason/EventsBeltReviewer/EventsBeltReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.EventsBeltReviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsBeltReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3640b6f628e4be08efbfa96021450448271e25e8
Jason Cothran <healingfox115@gmail.com>
2021-09-16 10:11:28 -0500

    Changed package & dependencies
    setup

--- src/main/java/com/Money/EventsBeltReviewer/EventsBeltReviewerApplication.java
+++ src/main/java/com/Money/EventsBeltReviewer/EventsBeltReviewerApplication.java
@@ -1,4 +1,4 @@
-package com.jason.EventsBeltReviewer;
+package com.Money.EventsBeltReviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
